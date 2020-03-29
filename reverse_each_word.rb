257feee4c0a91cc9e8a409614fddba3fb661cdb2
Toni  Diep <adiep123@gmail.com>
2020-03-29 21:33:49 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(sentence)
+  sentence << sentence.each() || sentence.split
+  end
+
+
+
+  #def reverse_each_word(sentence)
+  #turn the sentence into an array
+  #iterate over each word
+  #reverse each word
+  #turn the array back into a sentence .collect
+  #end
