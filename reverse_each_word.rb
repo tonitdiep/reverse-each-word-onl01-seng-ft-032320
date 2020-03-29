c0958dd4d454bc363661274a7f278351aa0b96a9
Toni  Diep <adiep123@gmail.com>
2020-03-29 23:22:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
 def reverse_each_word(sentence)
-  array1 = sentence.reverse
+  array1.collect do |word|
+     "Hello there, how are you?"
+  array2.reverse do |word| <<  "Hello there, how are you?"
+
+
   array2 = sentence.split
   array1 << sentence.collect 
-
   #def reverse_each_word(sentence)
   #turn the sentence into an array
   #iterate over each word
